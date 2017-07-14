d341e9960394ad2da65d98cefddbe8c4f74ed943
wanghlGitHub <568227120@qq,com>
2017-07-14 10:43:08 +0800

    鑫达24字方针项目

---
+++ src/com/seeyon/apps/xd/po/TargetPo.java
@@ -0,0 +1,28 @@
+package com.seeyon.apps.xd.po;
+
+import org.apache.log4j.Logger;
+
+import com.seeyon.ctp.common.AppContext;
+import com.seeyon.ctp.menu.check.AbstractMenuCheck;
+/**
+ * 战略规划 菜单校验类
+ * @author heliang.wang
+ *
+ * @time 2017年7月13日
+ */
+public class TargetPo extends AbstractMenuCheck{
+
+	private static final Logger LOGGER = Logger.getLogger(TargetPo.class);
+
+	@Override
+	public boolean check(long memberId, long loginAccountId) {
+//		String dbId = AppContext.getSystemProperty("trlaobao.laobaoAdmin");
+//		long dbMemberId = Long.parseLong(dbId);
+//		if(memberId == dbMemberId){
+//			LOGGER.info("劳保信息列表：" + memberId);
+//			return true;
+//		}
+		return true;
+	}
+	
+}
