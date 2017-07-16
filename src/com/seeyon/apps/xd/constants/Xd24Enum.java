348a2d9da73b554cb819a7c9f6e71db26ba95950
DELL <DELL@fengkai-7566>
2017-07-16 15:22:54 +0800

    tijiao

--- src/com/seeyon/apps/xd/constants/Xd24Enum.java
+++ src/com/seeyon/apps/xd/constants/Xd24Enum.java
@@ -1,15 +1,91 @@
 package com.seeyon.apps.xd.constants;
 
 public class Xd24Enum {
-	
+	/**
+	 * 目标录入--是否删除
+	 * @author heliang.wang
+	 *
+	 * @time 2017年7月16日
+	 */
 	public enum Target{
-	    DELETE_Y("0","删除"),
-	    DELETE_N("1","未删除");
+	    DELETE_Y("1","删除"),
+	    DELETE_N("0","未删除");
 	    
 	    private String key;
 	    private String text;
 	    
 	    Target(String key,String text){
+	        this.setKey(key);
+	        this.setText(text);
+	    }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+	}
+	
+	/**
+	 * 指标分类枚举
+	 * @author DELL
+	 *
+	 */
+	public enum Quota{
+	    QUOTACLASS_A("0","财务指标"),
+	    QUOTACLASS_B("1","间接财务指标"),
+	    QUOTACLASS_C("2","业务指标");
+	    
+	    private String key;
+	    private String text;
+	    
+	    Quota(String key,String text){
+	        this.setKey(key);
+	        this.setText(text);
+	    }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+	}
+	/**
+	 * 规划计划审批状态
+	 * @author heliang.wang
+	 *
+	 * @time 2017年7月16日
+	 */
+	public enum PlanEnum{
+	    PLAN_DISAPPROVE("0","未审批"),
+	    PLAN_APPROVEING("1","审批中"),
+	    PLAN_APPROVE_PASS("2","审批通过"),
+	    PLAN_APPROV_NOT_PASS("3","审批未通过");
+	    
+	    private String key;
+	    private String text;
+	    
+	    PlanEnum(String key,String text){
 	        this.setKey(key);
 	        this.setText(text);
 	    }
